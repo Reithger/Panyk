8c9d593813379e5f4a5b292fe4c8ea26ec26c853
Nick8675309 <46874760+Nick8675309@users.noreply.github.com>
2019-02-27 14:27:06 -0400

    Update Diary.java

--- model/trip/feature/Diary.java
+++ model/trip/feature/Diary.java
@@ -1,5 +1,34 @@
 package model.trip.feature;
 
-public class Diary implements Feature{
+import java.util.ArrayList;
 
+public class Diary implements Feature
+{
+	ArrayList<Entry> pages = new ArrayList<Entry>();
+	
+	public Diary(ArrayList<Entry> entries)
+	{
+		pages = entries;
+	}
+	
+	public void add(Entry e)
+	{
+		pages.add(e);
+	}
+	
+	public void delete(Entry e)
+	{
+		pages.remove(e);
+	}
+	
+	public ArrayList<Entry> getEntries()
+	{
+		return pages;
+	}
+	
+	public void setEntries(ArrayList<Entry> ale)
+	{
+		pages = ale;
+	}
+	
 }
