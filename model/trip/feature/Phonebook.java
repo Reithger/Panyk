c40577435faf3652df3e817134ee6afdbab718c6
Nick8675309 <46874760+Nick8675309@users.noreply.github.com>
2019-02-27 14:27:38 -0400

    Update Phonebook.java

--- model/trip/feature/Phonebook.java
+++ model/trip/feature/Phonebook.java
@@ -1,5 +1,40 @@
 package model.trip.feature;
 
-public class Phonebook implements Feature{
+import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
+import model.trip.logistic.Contact;
+
+public class Phonebook implements Feature
+{
+	ArrayList<Contact> contacts = new ArrayList<Contact>();
+	public Phonebook(ArrayList<Contact> tripPeople)//constructor for Phonebook class
+	{
+		contacts = tripPeople;
+	}
+	
+	public void add(Contact c)
+	{
+		if(contacts.contains(c))
+		{
+			JOptionPane.showMessageDialog(null, "You already have that contact, so cannot add it again.", "Duplicate Contact", JOptionPane.ERROR_MESSAGE);
+		}
+		else
+		{
+			contacts.add(c);
+		}
+		
+	}
+	
+	public void delete(Contact c)
+	{
+		contacts.remove(c);
+	}
+	
+	public ArrayList<Contact> getContacts()
+	{
+		return contacts;
+	}
+	
 }
