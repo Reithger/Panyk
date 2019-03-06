a002485fd2f8e7b4a469375a475bcf5ebb355650
Nick8675309 <46874760+Nick8675309@users.noreply.github.com>
2019-03-06 16:26:24 -0400

    Update Appointment.java

--- model/trip/schedule/Appointment.java
+++ model/trip/schedule/Appointment.java
@@ -1,107 +1,130 @@
 package model.trip.schedule;
 
 import java.util.Date;
+import java.util.HashMap;
+
+import model.trip.logistic.Contact;
 import model.trip.logistic.ProtoAddress;
 
 /**
- * A class to replace Schedulable. In coding schedulable's implementing classes weren't different enough
- * to need different classes, and their enumerations were likewise of little use
+ * A class to replace Schedulable. Other schedule objects will just be wrappers around appointment objects
  * 
  * @author nrcuthbertson
  *
  */
-
 public class Appointment {
 
-//---  Instance Variables   -------------------------------------------------------------------
-	
-	/** */
 	private String title;
-	/** */
 	private Date start;
-	/** */
 	private Date end;
-	/** */
 	private String type;
-	/** */
 	private String comments;
-	/** */
 	private float price;
-	/** */
 	private ProtoAddress location;
+	private Contact mycont;
 	
-//---  Constructors   -------------------------------------------------------------------------
-	
-	public Appointment(String called, Date begin, Date cease, String description, String userComments, float cost, ProtoAddress pa){
+	public Appointment(String called, Date begin, Date cease, Contact person, String description, String userComments, float cost, ProtoAddress pa)
+	{
 		if(called.equals(null))
-			called = "untitled";
-		else
-			title = called;
-		start = begin;
-		end = cease;
-		type = description;
-		comments = userComments;
-		price = cost;
-		location = pa;
+		{
+			called="untitled";
+		}
+		title=called;
+		start=begin;
+		end=cease;
+		type=description;
+		comments=userComments;
+		price=cost;
+		location=pa;
+		mycont=person;
 	}
 	
-//---  Getter Methods   -----------------------------------------------------------------------
+	/*public String getType()
+	{
+		return type;
+	}*/
 	
-	public Date getStartDate(){
+	public void setType(String newType)
+	{
+		type=newType;
+	}
+	
+	public Contact getContact()
+	{
+		return mycont;
+	}
+	
+	public void setContact(Contact newcont)
+	{
+		mycont=newcont;
+	}
+	
+	public Date getStartDate() 
+	{
 		return start;
 	}
 
-	public Date getEndDate(){
+	public Date getEndDate() 
+	{
 		return end;
 	}
 
-	public String getType(){
+	public String getType() 
+	{
 		return type;
 	}
+
+	public void setStartDate(Date in) 
+	{
+		start=in;
+		
+	}
+
+	public void setEndDate(Date d) 
+	{
+		end=d;
+		
+	}
 	
-	public String getComments(){
+	public String getComments() 
+	{
 		return comments;
 	}
 
-	public String getTitle(){
-		return title;
-	}
-	
-	public float getPrice(){
-		return price;
+	public void setComments(String commentary) {
+		comments=commentary;
+		
 	}
 
-	public ProtoAddress getLocation(){
+	public String getTitle() 
+	{
+		return title;
+	}
+
+	public void setTitle(String name) 
+	{
+		title=name;
+		
+	}
+	
+	public float getPrice()
+	{
+		return price;
+	}
+	
+	public void setPrice(float f)
+	{
+		price=f;
+	}
+	
+	public ProtoAddress getLocation()
+	{
 		return location;
 	}
 	
-//---  Setter Methods   -----------------------------------------------------------------------
-	
-	public void setStartDate(Date in){
-		start = in;
-		
-	}
-
-	public void setEndDate(Date d){
-		end = d;
-		
-	}
-
-	public void setComments(String commentary){
-		comments = commentary;
-		
-	}
-
-	public void setTitle(String name){
-		title = name;
-	}
-	
-	public void setPrice(float f){
-		price = f;
-	}
-	
-	public void setLocation(ProtoAddress newLoc){
-		location = newLoc;
+	public void setLocation(ProtoAddress newLoc)
+	{
+		location=newLoc;
 	}
 	
 }
