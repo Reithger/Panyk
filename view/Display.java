d89aee3242d8b9f9eebe8fb43571ac12218ab1cc
Regan Lynch <45275820+ReganLynch@users.noreply.github.com>
2019-03-10 01:59:57 -0400

    updated display
    display is not integrated with controller and database. Login screen has option to create a user, and once created, can be signed in anytime the app is opened.

--- view/Display.java
+++ view/Display.java
@@ -2,10 +2,14 @@ package view;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.util.Calendar;
 
 import input.Communication;
 import visual.frame.WindowFrame;
 import visual.panel.ElementPanel;
+import visual.panel.element.*;
+
+import controller.*;
 
 /**
  * This class is the core of the View; Intermediary (the Controller) communicates with this
@@ -34,8 +38,12 @@ public class Display {
 	private final static Font FONT_TWO = new Font("SansSerif", Font.BOLD, 40);
 	/** */
 	private final static Font FONT_ENTRY = new Font("Arial Bold", Font.BOLD, 12);
+	
+	//------------------------------------------
 	/** */
 	private final static Color COLOR_ONE = new Color(30, 80, 175);
+	/** */
+	private final static Color COLOR_LOGIN = new Color(102, 255, 102);
 	/** */
 	private final static Color COLOR_TWO = new Color(90, 80, 175);
 	/** */
@@ -43,7 +51,24 @@ public class Display {
 	/** */
 	private final static Color COLOR_BLACK = new Color(0, 0, 0);
 	/** */
-	private final static int EVENT_LOGIN = 1;
+	private final static Color COLOR_ERR = new Color(255, 80, 80);
+	/** */
+	private final static Color COLOR_SEPARATOR = new Color(255, 153, 0);
+	/** */
+	private final static Color COLOR_CREATE_ACC_BOX = new Color(204, 0, 102);
+	
+	//---------------------------
+	/** */
+	private final static int EVENT_GO_TO_LOGIN = 1;
+	/** */
+	private final static int EVENT_LOGIN = 2;
+	/** */
+	private final static int EVENT_CREATE_ACC_BTN = 3;
+	/** */
+	private final static int EVENT_CREATE_ACC_FINALIZE = 4;
+	/** */
+	private final static int EVENT_BACK_TO_LOGIN = 5;
+	
 
 //---  Instance Variables   -------------------------------------------------------------------
 	
@@ -53,8 +78,20 @@ public class Display {
 	private int width;
 	/** */
 	private int height;
-	/** */
-	private int elementCount;
+	
+	private boolean incorrectPassword;
+	
+	private boolean incorrectUsername;
+	
+	private boolean invalidFirstName;
+
+	private boolean invalidLastName;
+	
+	private boolean invalidUsername;
+	
+	private boolean invalidPassword;
+	
+	private boolean invalidDOB;
 	
 //---  Constructors   -------------------------------------------------------------------------
 	
@@ -70,7 +107,6 @@ public class Display {
 		width = inWidth;
 		height = inHeight;
 		display = new WindowFrame(width, height);
-		elementCount = 0;
 		initialScreen();
 	}
 	
@@ -83,26 +119,23 @@ public class Display {
 	 * 
 	 */
 	
-	private void initialScreen() {
+	private void initialScreen() 
+	{
 		ElementPanel titlePanel = new ElementPanel(0, 0, display.getWidth(), display.getHeight()) {
 			public void clickBehaviour(int event) {
-				if(event == EVENT_LOGIN) {
-					Communication.set("Control", "next");
+				if(event == EVENT_GO_TO_LOGIN) {
+					display.removePanel("Title");
+					logInScreen();
 				}
-			}
-			
+			}	
 		};
-		
 		titlePanel.addRectangle("rect1", 0, 0, 0, titlePanel.getWidth(), titlePanel.getHeight(), COLOR_ONE, false);
 		titlePanel.addRectangle("rect2", 5, width/20, height/12, width*18/20, 9*height/12, new Color(200,150,170), false);
 		titlePanel.addText("tex1", 15, width/2, height / 3, width/4, height/5, "Plein Air", FONT_TWO, true);
 		titlePanel.addRectangle("rect3", 14, width/2, 3*height/5, width/10, height/20, COLOR_WHITE, true);
-		titlePanel.addButton("but1", 15, width/2, 3*height/5, width/20, height/20, EVENT_LOGIN, true);
-		/*
-		titlePanel.addText("text1", 2, width/2, height/2, width/5, height/5, "Welcome", FONT_ONE, true);
-		titlePanel.addButton("but1", 5, width/2, height/2, width/5, height/10, COLOR_TWO, EVENT_LOGIN, true);
-		titlePanel.addText("text2", 10, width/2, height/2, width/5, height/10, "Log In", FONT_ONE, true);
-		*/
+		titlePanel.addButton("but1",     15, width/2, 3*height/5, width/20, height/20, EVENT_GO_TO_LOGIN, true);
+		titlePanel.addText("text_but1",  16, width/2, 3*height/5 + 5,  width/20, height/20, "start", FONT_ENTRY, true);
+
 		display.addPanel("Title", titlePanel);
 	}
 
@@ -113,14 +146,206 @@ public class Display {
 	 */
 	
 	public void logInScreen() {
-		display.hidePanel("Title");
-		ElementPanel login = new ElementPanel(0, 0, width, height);
+		ElementPanel login = new ElementPanel(0, 0, width, height) {
+			public void clickBehaviour(int event) {
+				if(event == EVENT_LOGIN) {
+					DrawnTextArea area_username = (DrawnTextArea) this.getElement("text_username");
+					DrawnTextArea area_password = (DrawnTextArea) this.getElement("text_password");
+					
+					String uname = area_username.getText();
+					String pass = area_password.getText();
+					
+					uname = removeFrontSpace(uname);
+					pass = removeFrontSpace(pass);
+					
+					//if user name and password are in DB
+					if(ViewLoginQuery.validUser(uname, pass)) {
+						System.out.println("username and password are good");
+						incorrectPassword = false;
+						incorrectUsername = false;
+					}
+					//if only user name is in DB (ie: password is incorrect)
+					else if(ViewLoginQuery.usernameExists(uname)) {
+						System.out.println("username exists -> password is wrong");
+						incorrectUsername = false;
+						incorrectPassword = true;
+						display.removePanel("Login");
+						logInScreen();
+					}
+					//otherwise if user name and password do not exist
+					else {
+						System.out.println("user does not exist");
+						incorrectPassword = false;
+						incorrectUsername = true;
+						display.removePanel("Login");
+						logInScreen();
+					}	
+				}else if(event == EVENT_CREATE_ACC_BTN) {
+					incorrectPassword = false;
+					incorrectUsername = false;
+					display.removePanel("Login");
+					createAccountScreen();
+				}
+			}
+		};
 		login.addRectangle("rect1", 0, 0, 0, width, height, COLOR_ONE, false);
-		login.addRectangle("rect2", 3, width/2, height/3, width/2, height/3, COLOR_WHITE, COLOR_BLACK, true);
-		login.addText("tex1", 5, width/2, height/3, width/2, height/3, "Log In", FONT_TWO, true);
-		designTextField(login, width/2, height/2 + height/6, width/6, height/12, 10, 1, true);
-		designTextField(login, width/2, height/2 + height/3, width/6, height/12, 10, 2, true);
+		login.addRectangle("rect2", 3, width/3, height/3, width/2, height/4, COLOR_WHITE, COLOR_BLACK, true);
+		login.addText("tex1", 5, width/3, height/3 + 40, width/2, height/3, "Log In", FONT_TWO, true);
+		//add username and password entries
+		//username
+		designTextField(login, "username", width/3, height/2 + 40, width/6, height/12, 10, 10001, true);
+		login.addText("tex2", 78,          width/3, height/2 + 10, width/6, height/12, "Username", FONT_ONE, true);
+		//password
+		designTextField(login, "password", width/3, height/2 + 130, width/6, height/12, 10, 10000, true);
+		login.addText("tex3", 79,          width/3, height/2 + 100, width/6, height/12, "Password", FONT_ONE, true);
+		//add button to login
+		login.addRectangle("but1_rect", 14, width/3, height - 100, width/8, height/20,  COLOR_LOGIN , true);
+		login.addText("but1_text", 15, width/3, height - 100, width/9, height/20 - 10, "log in!", FONT_ENTRY, true);
+		login.addButton("but1", 15, width/3, height - 100, width/9, height/20, EVENT_LOGIN, true);
+		//add create a user on the side
+		login.addRectangle("ver_bar", 24, 2*width/3, 0, 5, height, COLOR_SEPARATOR, false);
+		login.addRectangle("no_acc_rect", 25, 2*width/3 + 65, 150, 200, 50, COLOR_CREATE_ACC_BOX, false);
+		login.addText("no_acc_text", 26, 2*width/3 + 90, 165, 200, 50, "Don't have an account?", FONT_ENTRY, false);
+		login.addRectangle("no_acc_create_rect", 25, 2*width/3 + 115, 250, 100, 30, COLOR_LOGIN, false);
+		login.addText("no_acc_text_btn",         27, 2*width/3 + 130, 255, 100, 30, "create one!", FONT_ENTRY, false);
+		login.addButton("create_acc_btn", 28, 2*width/3 + 130, 255, 100, 30, EVENT_CREATE_ACC_BTN, false);
+		//check if password is incorrect
+		if(this.incorrectPassword) {
+			login.addRectangle("rect_incorrect_pass", 20, width/3 + 100, height/2 + 115, 90, 30,  COLOR_ERR, false);
+			login.addText(	   "text_incorrect_pass", 21, width/3 + 120, height/2 + 112, 90, 30, "Incorrect password", FONT_ENTRY, false);
+		}
+		//check if username is valid
+		if(this.incorrectUsername) {
+			login.addRectangle("rect_incorrect_user", 22, width/3 + 100, height/2 + 25 , 90, 30,  COLOR_ERR, false);
+			login.addText(	   "text_incorrect_user", 23, width/3 + 120, height/2 + 23 , 90, 30, "Invalid username", FONT_ENTRY, false);
+		}
 		display.addPanel("Login", login);
+	}
+	
+//------------------------------------------------------------------------------------------------------------------------------
+	public void createAccountScreen() {
+		ElementPanel createAcc = new ElementPanel(0, 0, width, height) {
+			public void clickBehaviour(int event) {
+				if(event == EVENT_CREATE_ACC_FINALIZE) {
+					//create the user based on passed in information
+					DrawnTextArea area_firstname = (DrawnTextArea) this.getElement("text_firstname");
+					DrawnTextArea area_lastname = (DrawnTextArea) this.getElement("text_lastname");
+					DrawnTextArea area_username = (DrawnTextArea) this.getElement("text_username");
+					DrawnTextArea area_password = (DrawnTextArea) this.getElement("text_password");
+					DrawnTextArea area_dob = (DrawnTextArea) this.getElement("text_dob");
+					
+					String fn    = area_firstname.getText();
+					String ln    = area_lastname.getText();
+					String dob   = area_dob.getText();
+					String uname = area_username.getText();
+					String pass  = area_password.getText();
+					
+					fn = removeFrontSpace(fn);
+					ln = removeFrontSpace(ln);
+					dob = removeFrontSpace(dob);
+					uname = removeFrontSpace(uname);
+					pass = removeFrontSpace(pass);
+					
+					//check validity of first name
+					if(fn.isEmpty()) {
+						invalidFirstName = true;
+					}else {
+						invalidFirstName = false;
+					}
+					//check validity of last name
+					if(ln.isEmpty()) {
+						invalidLastName = true;
+					}else {
+						invalidLastName = false;
+					}
+					//check valid DOB
+					if(dob.isEmpty() || !validDOB(dob)) {
+						invalidDOB = true;
+					}else {
+						invalidDOB = false;
+					}
+					//check validity of username
+					if(uname.isEmpty() || ViewLoginQuery.usernameExists(uname)) {
+						invalidUsername = true;
+					}else {
+						invalidUsername = false;
+					}
+					//check validity of password
+					if(pass.isEmpty()) {
+						invalidPassword = true;
+					}else {
+						invalidPassword = false;
+					}
+					//remove old frame
+					display.removePanel("Create Account");
+					//finally, check if everything passed
+					if(invalidFirstName || invalidLastName || invalidUsername || invalidPassword || invalidDOB) {
+						createAccountScreen();
+					}else {
+						//create account now
+						ObjectDBCreator.createUser(fn, ln, uname, pass, dob);
+						logInScreen();
+					}
+				}else if(event == EVENT_BACK_TO_LOGIN) {
+					display.removePanel("Create Account");
+					invalidFirstName = false;
+					invalidLastName = false;
+					invalidUsername = false;
+					invalidPassword = false;
+					invalidDOB = false;
+					logInScreen();
+				}
+			}
+		};
+		//backround
+		createAcc.addRectangle("backround", 0, 0, 0, width, height, COLOR_ONE, false);
+		//title
+		createAcc.addText("create_acc_title", 1, width/2, 65, width, height/5, "Create your account", FONT_TWO, true);
+		//first name
+		createAcc.addText("fn", 2, width/2, 130, 200, 100, "First name", FONT_ONE, true);
+		designTextField(createAcc, "firstname", width/2, 130, 200, 30, 3, 10002, true);
+		if(invalidFirstName) {
+			createAcc.addRectangle("rect_inv_fn",    13, width/2 + 115, 130 - 15, 100, 30, COLOR_ERR, false);
+			createAcc.addText("text_inv_fn", 14, width/2 + 140, 130 - 15, 100, 30, "Invalid first name", FONT_ENTRY, false);
+		}
+		//last name
+		createAcc.addText("ln", 4, width/2, 200, 200, 100, "Last name", FONT_ONE, true);
+		designTextField(createAcc, "lastname", width/2, 200, 200, 30, 5, 10003, true);
+		if(invalidLastName) {
+			createAcc.addRectangle("rect_inv_ln", 15, width/2 + 115, 200 - 15, 100, 30, COLOR_ERR, false);
+			createAcc.addText(     "text_inv_ln", 16, width/2 + 140, 200 - 15, 100, 30, "Invalid last name", FONT_ENTRY, false);
+		}
+		//DOB
+		createAcc.addText("dob", 4, width/2, 270, 500, 100, "Date of birth (YYYY-MM-DD)", FONT_ONE, true);
+		designTextField(createAcc, "dob", width/2, 270, 200, 30, 5, 10007, true);
+		if(invalidDOB) {
+			createAcc.addRectangle("rect_inv_dob", 22, width/2 + 115, 270 - 15, 100, 30, COLOR_ERR, false);
+			createAcc.addText(     "text_inv_dob", 23, width/2 + 130, 270 - 10, 100, 30, "Invalid DOB", FONT_ENTRY, false);
+		}
+		//username
+		createAcc.addText("uname", 6, width/2, 340, 200, 100, "Username", FONT_ONE, true);
+		designTextField(createAcc, "username", width/2, 340, 200, 30, 7, 10004, true);
+		if(invalidUsername) {
+			createAcc.addRectangle("rect_inv_un", 17, width/2 + 115, 340 - 15, 100, 30, COLOR_ERR, false);
+			createAcc.addText(     "text_inv_un", 18, width/2 + 140, 340 - 15, 100, 30, "Invalid username", FONT_ENTRY, false);
+		}
+		//password
+		createAcc.addText("pass", 8, width/2, 410, 200, 100, "Password", FONT_ONE, true);
+		designTextField(createAcc, "password", width/2, 410, 200, 30, 9, 10005, true);
+		if(invalidPassword) {
+			createAcc.addRectangle("rect_inv_pw", 19, width/2 + 115, 410 - 15, 100, 30, COLOR_ERR, false);
+			createAcc.addText(     "text_inv_pw", 20, width/2 + 140, 410 - 15, 100, 30, "Invalid password", FONT_ENTRY, false);
+		}
+		//create account button
+		createAcc.addRectangle("but_rect", 10, width/2, height - 110, width/8, height/20,  COLOR_LOGIN , true);
+		createAcc.addText("but_text",      11, width/2, height - 110, width, height/20 - 10, "Create my account!", FONT_ENTRY, true);
+		createAcc.addButton("create_but",  12, width/2, height - 110, width/9, height/20, EVENT_CREATE_ACC_FINALIZE, true);
+		//create a back button
+		createAcc.addRectangle("btn_back_rect", 26, 50, height - 100, 90, 30,  COLOR_ERR , false);
+		createAcc.addText("but_back_text",      27, 80, height - 95,  90, 30, "back", FONT_ENTRY, false);
+		createAcc.addButton("back_btn",         28, 50, height - 100, 90, 30, EVENT_BACK_TO_LOGIN, false);
+		
+		display.addPanel("Create Account", createAcc);
 	}
 
 //---  Composite Methods   --------------------------------------------------------------------
@@ -141,10 +366,46 @@ public class Display {
 	 * @param code
 	 */
 	
-	private void designTextField(ElementPanel pan, int x, int y, int panWid, int panHei, int priority, int code, boolean centered) {
-		pan.addRectangle("rec_" + elementCount++, priority * 10, x, y, panWid + 10, panHei, COLOR_WHITE, COLOR_BLACK, centered);
-		pan.addTextEntry("tex_in_" + elementCount++, priority * 10 + 1, x, y, panWid, panHei, code, FONT_ENTRY, centered);
-		
+	private void designTextField(ElementPanel pan,String name, int x, int y, int panWid, int panHei, int priority, int code, boolean centered) {
+		pan.addRectangle("rect_" + name, priority * 10, x, y, panWid + 10, panHei, COLOR_WHITE, COLOR_BLACK, centered);
+		pan.addTextEntry("text_" + name , priority * 10 + 1, x, y, panWid, panHei, code, FONT_ENTRY, centered);	
+	}
+	
+	//---------------------------------------------------------------------------------------------
+	
+	/** determines if a string (dob) is in the format YYYY-MM-DD
+	 * 
+	 * @param dob
+	 * @return
+	 */
+	private static boolean validDOB(String dob) {
+		String[] split = dob.split("-");
+		if(split.length != 3) {
+			return false;
+		}
+		try {
+			int year = Integer.parseInt(split[0]);
+			int month = Integer.parseInt(split[1]);
+			int day = Integer.parseInt(split[2]);
+			if(month > 12 || day > 31 || year > Calendar.getInstance().get(Calendar.YEAR)) {		//if the year is greater than the current year
+				return false;
+			}
+		}catch(Exception e) {
+			return false;
+		}
+		return true;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//wierd issues with spaces in front of words
+	public static String removeFrontSpace(String str) {
+		if(str != null) {
+			if(str.charAt(0) == ' ') {
+				str = str.substring(1);
+			}
+		}
+		return str;
 	}
 
 }
+
