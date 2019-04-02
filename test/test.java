cc39cfdc0d73bdca4aa846e9904c8570c129ea91
Sienna <smcollette@mta.ca>
2019-04-02 14:58:34 -0300

    cleaned up stuff no longer in use

--- test/test.java
+++ test/test.java
@@ -15,13 +15,7 @@ public class test {
 
 	public static void main(String[] args) {
 		String fonts[] = 
-			      GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-
-			    for ( int i = 0; i < fonts.length; i++ )
-			    {
-			      //System.out.println(fonts[i]);
-			    }
-		
+			      GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();		
 		Intermediary inter = new Intermediary();
 	}
 	
