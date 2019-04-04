722b5fb81fea6371c2b49bb978aa0363bbd6534b
Mac <39278228+Reithger@users.noreply.github.com>
2019-04-03 22:42:15 -0300

    Merge.

---
+++ model/trip/feature/Calendar.java
@@ -0,0 +1,21 @@
+package model.trip.feature;
+
+import java.util.HashMap;
+
+public class Calendar implements Feature{
+
+	
+	
+	@Override
+	public HashMap<String, String> exportDisplay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void exportMemory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
